60fac2bf124cf2c24a69ba29991b5c394d9f3492
Stephanomejia17 <stephano.mejia@outlook.es>
2023-08-01 19:39:31 -0500

    Implementacion de codigo para Ejercicio 13

---
+++ ejerciciosrepaso/ejericio_13.py
@@ -0,0 +1,4 @@
+numero_1 = int(input("Ingrese el primer numero: "))
+numero_2 = int(input("Ingrese el segundo numero: "))
+
+print("Suma: ", numero_1+numero_2,"\nResta: ", numero_1-numero_2,"\nMultiplicacion: ", numero_1*numero_2,"\nDivision: ", numero_1/numero_2)
